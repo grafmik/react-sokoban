b54a0a20802282ba5ccd25a88327150382d41539
Michael Graf <grafmik@gmail.com>
2015-10-22 14:54:17 +0200

    level data model with readable string and dynamic render

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 "use strict";
 
-var Game = React.createClass({
-    displayName: 'React Sokoban',
-    render: function() {
-    	return React.createElement(
-		   "div",
-		   { "className": "game" },
-		   React.createElement("div", { "className": "tile hero", style: {left:"5%",top:"5%"} }),
-		   React.createElement("div", { "className": "tile carton", style: {left:"10%",top:"5%"} }),
-		   React.createElement("div", { "className": "tile target", style: {left:"15%",top:"5%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"0%",top:"0%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"5%",top:"0%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"10%",top:"0%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"15%",top:"0%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"20%",top:"0%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"0%",top:"5%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"20%",top:"5%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"0%",top:"10%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"5%",top:"10%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"10%",top:"10%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"15%",top:"10%"} }),
-		   React.createElement("div", { "className": "tile wall", style: {left:"20%",top:"10%"} })
-		);
-    }
-});
+(function () {
+	var TILE_CSS = {
+		"H": "hero",
+		"O": "carton",
+		".": "target",
+		"X": "wall",
+		" ": ""
+	};
 
-ReactDOM.render(
-    React.createElement(Game),
-    document.getElementById('game')
-);
+	var getTileDim = function(level) {
+    	var levelRows = level.split("\n");
+		return {
+			w: Math.min(10, 100 / levelRows[0].length),
+			h: Math.min(10, 100 / levelRows.length)
+		}
+	};
 
+	var Game = React.createClass({
+	    displayName: 'React Sokoban',
+	    render: function() {
+	    	var levelRows = this.props.level.split("\n");
+	    	var tileDim = getTileDim(level);
+	        var createParams = ["div", {
+	            "className": "game"
+	        }];
+	        var tile = undefined, rowIndex = undefined, colIndex = undefined,
+	        	levelRow = undefined, tileType = undefined;
+	        for (rowIndex = levelRows.length - 1; rowIndex >= 0; rowIndex--) {
+	        	levelRow = levelRows[rowIndex];
+				for (colIndex = levelRow.length - 1; colIndex >= 0; colIndex--) {
+					tileType = levelRow[colIndex];
+					tile = React.createElement("div", { "className": "tile " + TILE_CSS[tileType],
+						style: {left:(tileDim.w*colIndex)+"%",top:(tileDim.h*rowIndex)+"%",
+						height: tileDim.h+"%",width: tileDim.w+"%"} });
+					createParams.push(tile);
+				};
+	        };
+	        return React.createElement.apply(this, createParams);
+	    }
+	});
+
+	var level = "" +
+	    "XXXXX\n" +
+	    "XHO.X\n" +
+	    "XXXXX";
+
+	ReactDOM.render(
+	    React.createElement(Game, {level: level}),
+	    document.getElementById('game')
+	);
+})(this);
