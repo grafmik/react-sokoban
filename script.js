ebb20ef53ce1e1e95e15dd7969241c46636bb256
Michael Graf <grafmik@gmail.com>
2015-10-22 18:37:10 +0200

    still a mock, with directions linked to keys, and new move functions to be implemented

--- script.js
+++ script.js
@@ -28,27 +28,59 @@
 			    ".XO"
 			};
 	    },
+	    _getHeroPosition: function(level) {
+	    	var levelRow = undefined, rowIndex = undefined, colIndex = undefined;
+	    	var levelRows = level.split("\n");
+	        for (rowIndex = levelRows.length - 1; rowIndex >= 0; rowIndex--) {
+	        	levelRow = levelRows[rowIndex];
+	        	colIndex = levelRow.indexOf("H");
+	        	if (colIndex >= 0) {
+	        		return {x:colIndex, y:rowIndex};
+	        	}
+	        }
+	    },
+	    _move: function(direction) {
+    		return "" +
+		    "XXXXX\n" +
+		    "X HOX\n" +
+		    "XXXXX";
+	    },
+	    _checkMove: function(direction) {
+	    	if (direction.x === 0 && direction.y === -1) {
+		    	return true;
+			}
+	    },
+	    _directions: {
+	    	'i': {x:0, y:-1},
+	    	'j': {x:-1, y:0},
+	    	'k': {x:0, y:1},
+	    	'l': {x:1, y:0}
+	    },
 	    _onFilterKeyPress:function(e) {
-	    	if (e.key === 'l') {
-				var level = "" +
-				    "XXXXX\n" +
-				    "X HOX\n" +
-				    "XXXXX";
+	    	var newLevel = undefined;
+	    	var direction = this._directions[e.key];
+	    	if (direction !== undefined) {
 	    		e.preventDefault();
-    			this.setState({level: level}, function()  {
-    				console.log("state changed");
-    			}.bind(this));
+	    		if (this._checkMove(direction)) {
+	    			newLevel = this._move(direction);
+		    		if (newLevel !== undefined) {
+						this.setState({currentLevel: newLevel}, function()  {
+							console.log("state changed");
+						}.bind(this));	    			
+		    		}
+	    		};
 	    	}
 	    },
 	    componentWillMount: function() {
 	    	if (this.props !== undefined && this.props.level !== undefined) {
-	    		this.setState({level: this.props.level});
+	    		this.setState({currentLevel: this.props.level, initialLevel: this.props.level,
+	    			heroPosition: this._getHeroPosition(this.props.level)});
 	    	}
 	    },
 	    render: function() {
 	        var result = undefined, tile = undefined, rowIndex = undefined, colIndex = undefined,
 	        	levelRow = undefined, tileType = undefined;
-	    	var levelRows = this.state.level.split("\n");
+	    	var levelRows = this.state.currentLevel.split("\n");
 	    	var tileDim = getTileDim(this.state.level);
 	        var createParams = ["div", {
 	            className: "game",
