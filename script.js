563fb2e48a6aa2e8cc43080bd07be8ec4d50466c
Michael Graf <grafmik@gmail.com>
2015-10-22 17:54:14 +0200

    key bindings with state change and re-render of a new mock level state

--- script.js
+++ script.js
@@ -19,14 +19,42 @@
 
 	var Game = React.createClass({
 	    displayName: 'React Sokoban',
+	    getInitialState: function() {
+			// impossible level - this should always been overriden by props
+			return {
+				level: "" +
+			    "OX.\n" +
+			    "XHX\n" +
+			    ".XO"
+			};
+	    },
+	    _onFilterKeyPress:function(e) {
+	    	if (e.key === 'l') {
+				var level = "" +
+				    "XXXXX\n" +
+				    "X HOX\n" +
+				    "XXXXX";
+	    		e.preventDefault();
+    			this.setState({level: level}, function()  {
+    				console.log("state changed");
+    			}.bind(this));
+	    	}
+	    },
+	    componentWillMount: function() {
+	    	if (this.props !== undefined && this.props.level !== undefined) {
+	    		this.setState({level: this.props.level});
+	    	}
+	    },
 	    render: function() {
-	    	var levelRows = this.props.level.split("\n");
-	    	var tileDim = getTileDim(level);
-	        var createParams = ["div", {
-	            "className": "game"
-	        }];
-	        var tile = undefined, rowIndex = undefined, colIndex = undefined,
+	        var result = undefined, tile = undefined, rowIndex = undefined, colIndex = undefined,
 	        	levelRow = undefined, tileType = undefined;
+	    	var levelRows = this.state.level.split("\n");
+	    	var tileDim = getTileDim(this.state.level);
+	        var createParams = ["div", {
+	            className: "game",
+	            tabIndex: 1,
+	            onKeyPress: this._onFilterKeyPress
+	        }];
 	        for (rowIndex = levelRows.length - 1; rowIndex >= 0; rowIndex--) {
 	        	levelRow = levelRows[rowIndex];
 				for (colIndex = levelRow.length - 1; colIndex >= 0; colIndex--) {
@@ -37,7 +65,8 @@
 					createParams.push(tile);
 				};
 	        };
-	        return React.createElement.apply(this, createParams);
+	        result = React.createElement.apply(this, createParams);
+	        return result;
 	    }
 	});
 
